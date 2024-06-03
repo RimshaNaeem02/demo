490d76f338cba4ec4b9fc494d3d5b95917af1a6c
RimshaNaeem02 <rimshanaeem@teamx.global>
2024-06-03 13:14:13 +0500

    video-call-agora

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AgoraUIKit from 'agora-react-uikit';
 
-function App() {
+const App = () => {
+  const [videoCall, setVideoCall] = useState(false);
+  const [isScreenSharing, setIsScreenSharing] = useState(false);
+
+  const rtcProps = {
+    appId: "8fe3c7e26b6849bd98669a9613c23389",
+    channel: "rimsha",
+    token: "007eJxTYIiV4ls+ha3Kf+6av+bcJj8rP4ZoWXPLiHKH1f+9HSd95ZMCg0VaqnGyeaqRWZKZhYllUoqlhZmZZaKlmaFxspGxsYWlUGpsWkMgI4OVtD0zIwMjAwsQg/hMYJIZTLKASTaGoszc4oxEBgYAlzMetg==",
+    enableVideo: true,
+    enableAudio: true,
+    displayUsername: true,
+    layout: 0,
+  };
+
+  const callbacks = {
+    EndCall: () => {
+      setVideoCall(false);
+      setIsScreenSharing(false);
+      // stopScreenShare();
+    },
+  };
+
+  const handleStartCall = () => {
+    setVideoCall(true);
+  };
+
+  const toggleScreenShare = () => {
+    if (isScreenSharing) {
+      //AgoraUIKit.stopScreenShare();
+    } else {
+      // AgoraUIKit.startScreenShare();
+    }
+    setIsScreenSharing(!isScreenSharing);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      color: "red",
+      display: 'flex',
+      width: "95vw",
+      height: "80vh",
+      margin: '10px auto',
+    }}>
+
+      {videoCall ? (
+        <>
+          <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+          {/* <button style={{
+color: "red",
+display: 'flex',
+width: "9vw",
+height: "80vh"
+}} className='btn' onClick={toggleScreenShare}>
+{isScreenSharing ? 'Stop Screen Share' : 'Start Screen Share'}
+</button> */}
+        </>
+      ) : (
+        <button style={{
+          color: "red",
+          display: 'flex',
+          width: "80px",
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: "22px"
+        }} className='btn' onClick={handleStartCall}>Start Call</button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
